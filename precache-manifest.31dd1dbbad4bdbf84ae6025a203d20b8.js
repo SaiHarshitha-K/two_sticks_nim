ff43f6c78e121e9f0ffa5f66e0b78384446a11b0
SaiHarshitha-K <harshithakomati@gmail.com>
2024-10-16 12:58:50 -0500

    Updates

---
+++ precache-manifest.31dd1dbbad4bdbf84ae6025a203d20b8.js
@@ -0,0 +1,62 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "8c1d53535b1f9cbadf6a54db67eb2ecd",
+    "url": "/two_sticks_nim/index.html"
+  },
+  {
+    "revision": "38206db2c762c9b52fec",
+    "url": "/two_sticks_nim/static/css/2.b1e21a25.chunk.css"
+  },
+  {
+    "revision": "0bc22d0d5287e3593b14",
+    "url": "/two_sticks_nim/static/css/main.65928e0f.chunk.css"
+  },
+  {
+    "revision": "38206db2c762c9b52fec",
+    "url": "/two_sticks_nim/static/js/2.74673cbd.chunk.js"
+  },
+  {
+    "revision": "28bc9d147a79373c4500",
+    "url": "/two_sticks_nim/static/js/3.62d5139d.chunk.js"
+  },
+  {
+    "revision": "0bc22d0d5287e3593b14",
+    "url": "/two_sticks_nim/static/js/main.4d21ae70.chunk.js"
+  },
+  {
+    "revision": "13d6b12b9e167812fd54",
+    "url": "/two_sticks_nim/static/js/runtime~main.6b7b04b9.js"
+  },
+  {
+    "revision": "56c8d80832e37783f12c05db7c8849e2",
+    "url": "/two_sticks_nim/static/media/fa-brands-400.56c8d808.ttf"
+  },
+  {
+    "revision": "715d593456fa02fe72a008a72398f5be",
+    "url": "/two_sticks_nim/static/media/fa-brands-400.715d5934.woff2"
+  },
+  {
+    "revision": "2d89b49ac28614e9ccd9c81308b5120c",
+    "url": "/two_sticks_nim/static/media/fa-regular-400.2d89b49a.woff2"
+  },
+  {
+    "revision": "370dd5af19f8364907b6e2c41f45dbbf",
+    "url": "/two_sticks_nim/static/media/fa-regular-400.370dd5af.ttf"
+  },
+  {
+    "revision": "237f4a0afbdb652fb2330ee7e1567dd3",
+    "url": "/two_sticks_nim/static/media/fa-solid-900.237f4a0a.woff2"
+  },
+  {
+    "revision": "adec7d6f310bc577f05e8fe06a5daccf",
+    "url": "/two_sticks_nim/static/media/fa-solid-900.adec7d6f.ttf"
+  },
+  {
+    "revision": "38fec6307db2547752379b51322ded10",
+    "url": "/two_sticks_nim/static/media/fa-v4compatibility.38fec630.ttf"
+  },
+  {
+    "revision": "7c428138d15b872f193c40828f6fa0d3",
+    "url": "/two_sticks_nim/static/media/fa-v4compatibility.7c428138.woff2"
+  }
+]);
